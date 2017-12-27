f363150421edc20a59adc3d0ee7f9c770ea25915
tigrazone <tigrazone@narod.ru>
2017-12-27 15:45:33 +0200

    added cmake files

---
+++ CMakeFiles/raytr.dir/DependInfo.cmake
@@ -0,0 +1,42 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/msys64/home/thr/raytr-master/bbox_tree.cpp" "C:/msys64/home/thr/raytr-master/CMakeFiles/raytr.dir/bbox_tree.cpp.obj"
+  "C:/msys64/home/thr/raytr-master/ext/SimpleObject.cpp" "C:/msys64/home/thr/raytr-master/CMakeFiles/raytr.dir/ext/SimpleObject.cpp.obj"
+  "C:/msys64/home/thr/raytr-master/ext/dSFMT/dSFMT.cpp" "C:/msys64/home/thr/raytr-master/CMakeFiles/raytr.dir/ext/dSFMT/dSFMT.cpp.obj"
+  "C:/msys64/home/thr/raytr-master/ext/tiny_obj_loader.cc" "C:/msys64/home/thr/raytr-master/CMakeFiles/raytr.dir/ext/tiny_obj_loader.cc.obj"
+  "C:/msys64/home/thr/raytr-master/image.cpp" "C:/msys64/home/thr/raytr-master/CMakeFiles/raytr.dir/image.cpp.obj"
+  "C:/msys64/home/thr/raytr-master/main.cpp" "C:/msys64/home/thr/raytr-master/CMakeFiles/raytr.dir/main.cpp.obj"
+  "C:/msys64/home/thr/raytr-master/meshsimp1.cpp" "C:/msys64/home/thr/raytr-master/CMakeFiles/raytr.dir/meshsimp1.cpp.obj"
+  "C:/msys64/home/thr/raytr-master/objekt.cpp" "C:/msys64/home/thr/raytr-master/CMakeFiles/raytr.dir/objekt.cpp.obj"
+  "C:/msys64/home/thr/raytr-master/objloader.cpp" "C:/msys64/home/thr/raytr-master/CMakeFiles/raytr.dir/objloader.cpp.obj"
+  "C:/msys64/home/thr/raytr-master/pathtracer.cpp" "C:/msys64/home/thr/raytr-master/CMakeFiles/raytr.dir/pathtracer.cpp.obj"
+  "C:/msys64/home/thr/raytr-master/photon_tree.cpp" "C:/msys64/home/thr/raytr-master/CMakeFiles/raytr.dir/photon_tree.cpp.obj"
+  "C:/msys64/home/thr/raytr-master/pm.cpp" "C:/msys64/home/thr/raytr-master/CMakeFiles/raytr.dir/pm.cpp.obj"
+  "C:/msys64/home/thr/raytr-master/primitives.cpp" "C:/msys64/home/thr/raytr-master/CMakeFiles/raytr.dir/primitives.cpp.obj"
+  "C:/msys64/home/thr/raytr-master/random.cpp" "C:/msys64/home/thr/raytr-master/CMakeFiles/raytr.dir/random.cpp.obj"
+  "C:/msys64/home/thr/raytr-master/scene.cpp" "C:/msys64/home/thr/raytr-master/CMakeFiles/raytr.dir/scene.cpp.obj"
+  "C:/msys64/home/thr/raytr-master/shade.cpp" "C:/msys64/home/thr/raytr-master/CMakeFiles/raytr.dir/shade.cpp.obj"
+  "C:/msys64/home/thr/raytr-master/spectrum.cpp" "C:/msys64/home/thr/raytr-master/CMakeFiles/raytr.dir/spectrum.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "NO_CV"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "ext"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/raytr.dir/cmake_clean.cmake
@@ -0,0 +1,30 @@
+file(REMOVE_RECURSE
+  "raytr_pch/stdafx.h"
+  "raytr_pch/stdafx.h.gch/.c++"
+  "CMakeFiles/raytr.dir/main.cpp.obj"
+  "CMakeFiles/raytr.dir/scene.cpp.obj"
+  "CMakeFiles/raytr.dir/primitives.cpp.obj"
+  "CMakeFiles/raytr.dir/pathtracer.cpp.obj"
+  "CMakeFiles/raytr.dir/random.cpp.obj"
+  "CMakeFiles/raytr.dir/image.cpp.obj"
+  "CMakeFiles/raytr.dir/photon_tree.cpp.obj"
+  "CMakeFiles/raytr.dir/pm.cpp.obj"
+  "CMakeFiles/raytr.dir/bbox_tree.cpp.obj"
+  "CMakeFiles/raytr.dir/ext/SimpleObject.cpp.obj"
+  "CMakeFiles/raytr.dir/ext/tiny_obj_loader.cc.obj"
+  "CMakeFiles/raytr.dir/objloader.cpp.obj"
+  "CMakeFiles/raytr.dir/shade.cpp.obj"
+  "CMakeFiles/raytr.dir/objekt.cpp.obj"
+  "CMakeFiles/raytr.dir/spectrum.cpp.obj"
+  "CMakeFiles/raytr.dir/ext/dSFMT/dSFMT.cpp.obj"
+  "CMakeFiles/raytr.dir/meshsimp1.cpp.obj"
+  "raytr.pdb"
+  "raytr.exe"
+  "raytr.exe.manifest"
+  "libraytr.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/raytr.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
